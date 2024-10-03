f021c72f079c0dacc65af8e31dc730acd9960245
langlangsatrio <langlangsatrio@gmail.com>
2024-10-03 21:37:48 +0700

    Day 3

--- day03.ts
+++ day03.ts
@@ -1,3 +1,17 @@
+/**
+ * 1:Solved
+ * 2:Solved
+ * 3:Solved
+ * 4:Unsolved
+ * 5:Solved
+ * 6:Unsolved
+ * 7:Solved
+ * 8:Solved
+ * 9:Solved
+ * 10:Solved
+ * 11:Solved
+ */
+
 /**-------------------------------------------- */
 
 
@@ -52,15 +66,14 @@ let currencyInput = 1000000;
 let currencyString = currencyInput.toString()
 console.log(currencyString);
 
-
-//Insert titik ke index 1
-//Insert titik setiap 3 index setelah index 1
-//Insert RP dan titik dua di akhir -> Bisa pakai template literal
+//Insert titik ke index 1 (replace? // slice??) --> 1000 => 1.000
+//Insert titik setiap 3 index setelah index 1 (while loop sampai karakter terakhir)
+//Insert RP,titik, dan dua angka nol di akhir -> Bisa pakai template literal
 
 /**-------------------------------------------- */
 
 
-//Number 5 : Search and Replace String
+//Number 5 : Search and Replace String [SOLVED]
 let defaultString = "hello world";
 let searchString = "orld";
 let replace = defaultString.replace(searchString, "");
@@ -89,7 +102,7 @@ for (let i = 0; i < inputCapitalLetter.length; i++) {
 
 console.log(inputCapitalLetterFormatted);
 
-//ANSWER FROM DISCUSSION
+//ANSWER FROM DISCUSSION [SOLVED]
 
 let sentenceCap: string = "hello new world";
 let resultCap: string = "";
@@ -108,7 +121,7 @@ console.log(resultCap);
 /**-------------------------------------------- */
 
 
-//Number 7 : Letter case switcher
+//Number 7 : Letter case switcher [SOLVED]
 
 let inputLetter = "pURwaDhiKa";
 let outputLetter = "";
@@ -132,7 +145,7 @@ console.log(outputLetter)
 /**-------------------------------------------- */
 
 
-//Number 8 : Find largest integer
+//Number 8 : Find largest integer [SOLVED]
 let numberA = 80;
 let numberB = 76;
 
@@ -147,33 +160,60 @@ if (numberA > numberB) {
 /**-------------------------------------------- */
 
 
-//Number 9 Sort three numbers
+//Number 9 Sort three numbers [SOLVED]
 
-let inputNumber1 = 99;
+let inputNumber1 = 9999;
 let inputNumber2 = 888;
-let inputNumber3 = 70;
-let resultSort = 0;
-let isEnd = false;
+let inputNumber3 = 70933;
+let penampung = ``;
 
+//-----Way 1------// [Fail]
 //Kalkulasi nilai terbesar antara total data (Math.max?)
 //Keluarkan nilai terbesar ke variabel penampung dan ulangi step pertama sampai data habis (while?)
+//Log variabel penampung
 
-
-
+//------Another way-----// [Fail]
 //Cari nilai terbesar dan terkecil
 //Cocokkan nilai terbesar
 
-let biggest = Math.max(inputNumber1, inputNumber2, inputNumber3);
-let smallest = Math.min(inputNumber1, inputNumber2, inputNumber3);
-console.log(biggest);
-console.log(smallest);
+// let biggest = Math.max(inputNumber1, inputNumber2, inputNumber3);
+// let smallest = Math.min(inputNumber1, inputNumber2, inputNumber3);
+// console.log(biggest);
+// console.log(smallest);
+// if (biggest === inputNumber1) {}
 
-if (biggest === inputNumber1) {}
+//-----Alternatif mentok-------//
+//Manual If-Else
+if (inputNumber1 > inputNumber2 && inputNumber1 > inputNumber3) {
+    penampung+=`${inputNumber1}, `
+    if (inputNumber2 > inputNumber3) {
+        penampung+=`${inputNumber2}, ${inputNumber3}`
+    } else {
+        penampung+=`${inputNumber3}, ${inputNumber2}`
+    } 
+} else if (inputNumber2 > inputNumber1 && inputNumber2 > inputNumber3) {
+     penampung+=`${inputNumber2}, `
+    if (inputNumber1 > inputNumber3) {
+        penampung+=`${inputNumber1}, ${inputNumber3}`
+    } else {
+        penampung+=`${inputNumber3}, ${inputNumber1}`
+    }    
+} else if (inputNumber3 > inputNumber1 && inputNumber3 > inputNumber2) {
+    penampung+=`${inputNumber3}, `
+    if (inputNumber2 > inputNumber1) {
+        penampung+=`${inputNumber2}, ${inputNumber1}`
+    } else {
+        penampung+=`${inputNumber1}, ${inputNumber2}`
+    }    
+}
+
+console.log(penampung);
+
 
 /**-------------------------------------------- */
 
 
-//Number 10 : Log based on input data
+//Number 10 : Log based on input data [SOLVED]
 
 let inputData1 = 99;
 
@@ -189,7 +229,7 @@ if (typeof inputData1 === 'string') {
 /**-------------------------------------------- */
 
 
-//Number 11 : Changing A into *
+//Number 11 : Changing A into * [SOLVED]
 let characterChangeInput = "An apple a day keeps the doctor away";
 
 for (let i = 0; i < characterChangeInput.length; i++) {
